60a6ada1e61fd0d40512d0a198fe813433e0c87d
Eduardo <edu.1990@hotmail.com>
2017-09-25 16:04:50 +0200

    create form for add new products

--- src/modules/add-products/addProducts.component.ts
+++ src/modules/add-products/addProducts.component.ts
@@ -1,7 +1,23 @@
 import { Component } from '@angular/core';
 
+import {Product} from './model/product';
+
 @Component({
 	selector: 'addProducts',
 	templateUrl: './addProducts.component.html'
 })
-export class AddProductsComponent{}
+export class AddProductsComponent{
+	public product: Product;
+
+	constructor(){
+		this.product = new Product(0,'','',0,'');
+	}
+
+	ngOnInit(){
+		console.log(this.product);
+	}
+
+	onSubmit(){
+		console.log(this.product);
+	}
+}
